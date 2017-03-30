bdeed70475165c1547df9155ca73496dc111a099
tanduping <tanduping@163.com>
2017-03-30 10:34:52 +0800

    Update Test.java

--- testjava/src/testjava/Test.java
+++ testjava/src/testjava/Test.java
@@ -5,7 +5,7 @@ public class Test {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		System.out.println(11);
-		System.out.println("网页修改");
+		System.out.println("网页修改2");
 	}
 
 }
